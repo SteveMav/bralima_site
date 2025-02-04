5b80a0f0b4fbc8397237d90c979bc28818bc57a4
steve mavuela <stevemavuela@gmail.com>
2025-02-04 15:02:27 +0100

    debut

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-
+from core.models import CompanyInfo
 # Create your views here.
 def index(request):
-    pass
+    Company_info = CompanyInfo.objects.all()
+    return render(request, 'main/index.html', {'Company_info': Company_info})
--- news/models.py
+++ news/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-# Create your models here.
-class ContactMessage(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    message = models.TextField()
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to="news/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Message from {self.name}"
+        return self.title
