d6da4ae568e3033660811639a4f7642c017199b9
steve mavuela <stevemavuela@gmail.com>
2025-02-14 02:09:32 +0100

    dd

--- main/views.py
+++ main/views.py
@@ -18,6 +18,11 @@ def price_list(request):
     return render(request, 'main/price_list.html', {'product_price': product_price})
 
 def gallery(request):
-    images = Image.objects.all()
-    return render(request, 'main/gallery.html', {'images': images})
-
+    images = Image.objects.all().order_by('event')
+    grouped_images = {}
+    for image in images:
+        event = image.event
+        if event not in grouped_images:
+            grouped_images[event] = []
+        grouped_images[event].append(image)
+    return render(request, 'main/gallery.html', {'grouped_images': grouped_images})
